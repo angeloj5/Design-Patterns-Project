8c871f94f053a3370506c85982242ee8a7bb818f
Angel <angeloj5@hotmail.com>
2022-11-09 16:35:39 -0600

    Adding the Adapter Pattern in .Net C#

---
+++ .net/desing-pattern-adapter/CsvToJsonAdapter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desing_pattern_adapter
+{
+    internal class CsvToJsonAdapter : IDisposable
+    {
+        List<string> MoCsvRows = null;
+        string MsColumnSeparator = ",";
+
+        public CsvToJsonAdapter(string PsCsvContent, string PsColumnSeparator, string PsRowSeparator)
+        {
+            if (string.IsNullOrEmpty(PsCsvContent))
+                throw new Exception("There is no data to convert.");
+
+            MsColumnSeparator = string.IsNullOrEmpty(PsColumnSeparator) ? MsColumnSeparator : PsColumnSeparator;
+
+            PsRowSeparator = string.IsNullOrEmpty(PsRowSeparator) ? "\r\n" : PsRowSeparator;
+
+            MoCsvRows = new List<string>(PsCsvContent.Split(PsRowSeparator));
+        }
+
+        public StringBuilder ToJson()
+        {
+            StringBuilder LoResult = new StringBuilder("[\r\n{\r\n");
+
+            foreach (string LsRow in MoCsvRows)
+            {
+                string[] LsContent = LsRow.Split(MsColumnSeparator);
+                for (int LiArrayIndex = 0; LiArrayIndex < LsContent.Length; LiArrayIndex++)
+                    LoResult.AppendLine("\"Column " + (LiArrayIndex + 1).ToString() + "\":\"" + LsContent[LiArrayIndex] + "\",");
+
+                LoResult.AppendLine("},{");
+            }
+            LoResult = LoResult.Replace(",{", String.Empty, LoResult.Length - 4, 2);
+            LoResult.Append(']');
+            return LoResult;
+        }
+
+        public void Dispose()
+        {
+            MoCsvRows = null;
+            MsColumnSeparator = null;
+
+            GC.SuppressFinalize(this);
+            
+            GC.Collect();
+        }
+    }
+}
---
+++ .net/desing-pattern-adapter/Program.cs
@@ -0,0 +1,11 @@
+﻿using desing_pattern_adapter;
+
+Console.WriteLine("This is an implemetation of the Adapter Desing Pattern.\n\r");
+Console.WriteLine("Reading CSV file...\n\r");
+string LsCsvContent = File.ReadAllText("./CsvFile.csv");
+Console.WriteLine("CSV Content: ");
+Console.WriteLine(LsCsvContent+ "\n\r");
+CsvToJsonAdapter LoAdapter = new CsvToJsonAdapter(LsCsvContent, ",", "\r\n");
+Console.WriteLine("Converting to JSON...\n\r\n\rPrinting Json result: ");
+Console.Write(LoAdapter.ToJson().ToString());
+LoAdapter.Dispose();
